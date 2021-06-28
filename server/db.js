2116826f61954e3f6b85a682d8e882c88acf0a07
SDToews <33942967+SDToews@users.noreply.github.com>
2021-06-28 10:48:25 -0700

    change db

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
 	user: 'postgres',
 	password: 'snowball',
-	host: 'localhost',
+	host: 'postgresql',
 	port: 5432,
 	database: 'perntodo'
 })
